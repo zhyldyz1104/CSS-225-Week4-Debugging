882068d5709727bf3e204e58fdcbc356f341e7a3
Zhyldyz Torogulova <103953367+zhyldyz1104@users.noreply.github.com>
2025-10-21 14:39:00 -0500

    Add files via upload

---
+++ pirate.py
@@ -0,0 +1,6 @@
+greeting = input("Hello, possible pirate! What's the password?")
+password = "Arrr!"
+if greeting == password:
+    print("Go away, pirate.")
+else:
+    print("Greetings, hater of pirates!")
