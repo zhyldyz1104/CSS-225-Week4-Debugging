afe3535440b4e30c4ccc79d22ab671cc46d79b6e
Zhyldyz Torogulova <103953367+zhyldyz1104@users.noreply.github.com>
2025-10-21 14:41:32 -0500

    Add files via upload

---
+++ time.py
@@ -0,0 +1,5 @@
+current_time_str = int(input("What is the current time (in hours 0-23)?"))
+wait_time_str = int(input("How many hours do you want to wait"))
+
+final_time = current_time_str + wait_time_str
+print(final_time)
